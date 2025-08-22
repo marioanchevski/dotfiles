8d57f2a142e14326a1a3989c7e26927659d58bc0
Mario Anchevski <marioancevski25@gmail.com>
2025-08-22 17:04:27 +0200

    nvim: conform

--- .config/nvim/lua/mariodev/plugins/lsp.lua
+++ .config/nvim/lua/mariodev/plugins/lsp.lua
@@ -1,41 +1,49 @@
 return {
   {
-    "j-hui/fidget.nvim", opts = {}
+    'j-hui/fidget.nvim',
+    opts = {},
   },
   {
-    "mason-org/mason-lspconfig.nvim",
+    'mason-org/mason-lspconfig.nvim',
     dependencies = {
-      "mason-org/mason.nvim", opts = {}
+      {
+        'mason-org/mason.nvim',
+        opts = {},
+      },
+      {
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        opts = { ensure_installed = { 'stylua' } },
+      },
     },
     opts = {
       ensure_installed = {
-        "lua_ls",
-        "gopls",
+        'lua_ls',
+        'gopls',
       },
       automatic_enable = false,
     },
     config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
+      vim.api.nvim_create_autocmd('LspAttach', {
         -- when this file gets sources make sure to clear all the defined
         -- autocmds for this group before running the callback
-        group = vim.api.nvim_create_augroup("LspGroup", { clear = true }),
+        group = vim.api.nvim_create_augroup('LspGroup', { clear = true }),
         callback = function(event)
           local map = function(mode, keys, func)
             vim.keymap.set(mode, keys, func, { buffer = event.buf })
           end
 
-          map("n", "gr", vim.lsp.buf.rename)
-          map("n", "<leader>gd", vim.lsp.buf.declaration)
-          map("n", "<leader>ca", vim.lsp.buf.code_action)
+          map('n', 'gr', vim.lsp.buf.rename)
+          map('n', '<leader>gd', vim.lsp.buf.declaration)
+          map('n', '<leader>ca', vim.lsp.buf.code_action)
+          map('n', '<leader>gr', require('telescope.builtin').lsp_references)
 
-          map("n", "<leader>gi", require("telescope.builtin").lsp_implementations)
-          map("n", "<leader>gr", require("telescope.builtin").lsp_references)
-          map("n", "gd", require("telescope.builtin").lsp_definitions)
-          map("n", "gO", require("telescope.builtin").lsp_document_symbols)
+          map('n', '<leader>gi', require('telescope.builtin').lsp_implementations)
+          map('n', 'gd', require('telescope.builtin').lsp_definitions)
+          map('n', 'gO', require('telescope.builtin').lsp_document_symbols)
         end,
       })
 
-      vim.lsp.enable({ "lua_ls", "gopls" })
-    end
-  }
+      vim.lsp.enable { 'lua_ls', 'gopls' }
+    end,
+  },
 }
