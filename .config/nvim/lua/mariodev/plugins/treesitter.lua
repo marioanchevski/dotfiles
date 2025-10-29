eb70e197c05b592544925ee14748f9dc28e4c330
Mario Anchevski <marioancevski25@gmail.com>
2025-10-29 22:00:28 +0100

    nvim: add treesitter language parsers

--- .config/nvim/lua/mariodev/plugins/treesitter.lua
+++ .config/nvim/lua/mariodev/plugins/treesitter.lua
@@ -1,29 +1,35 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
+  build = ':TSUpdate',
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require('nvim-treesitter.configs').setup {
       ensure_installed = {
-        "c",
-        "lua",
-        "html",
-        "javascript",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline",
-        "go",
-        "java",
+        'c',
+        'lua',
+        'html',
+        'css',
+        'json',
+        'javascript',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'query',
+        'markdown',
+        'markdown_inline',
+        'go',
+        'java',
+        'yaml',
+        'bash',
+        'dockerfile',
+        'gitignore',
       },
       highlight = {
         enable = true,
       },
       indent = {
         enabled = true,
-      }
+      },
     }
-  end
+  end,
 }
